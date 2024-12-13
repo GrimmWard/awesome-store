620c49e22c11a21c91bc054d46869ad77cf14437
GrimmWard <vanakramar76@gmail.com>
2024-12-13 20:03:41 +0200

    feat: create db, add Prisma
    Signed-off-by: GrimmWard <vanakramar76@gmail.com>

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '@/prisma/prisma.module';
+// import { AuthModule } from './auth/auth.module';
+// import { EmailModule } from './email/email.module';
 
 @Module({
-	imports: [],
+	// imports: [PrismaModule, AuthModule, EmailModule],
+	imports: [PrismaModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
